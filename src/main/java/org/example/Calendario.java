7f282b818626ac072c763a7e10b711d720094fdc
Eduardo Victorio <eduardovictorio@MacBook-Air-de-Eduardo.local>
2023-10-13 20:02:52 -0500

    Subo mi primer proyecto

---
+++ src/main/java/org/example/Calendario.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Calendario {
+    public static void main(String[] args) {
+        System.out.println("Diciembre");
+    }
+}
---
+++ src/main/java/org/example/Matematicas.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Matematicas {
+ 
+
+    public static void main(String[] args) {
+        Integer numero1 = 50;
+
+        Integer operacionMatematica = numero1 * 2;
+
+        System.out.println("operacionMatematica = " + operacionMatematica);
+    }
+}
